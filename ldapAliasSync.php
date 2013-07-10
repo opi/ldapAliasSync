138eb7ed4ec86aa87c24b2bc2d41bf85f1225932
Lukas Mika <lukas.mika@web.de>
2013-07-10 10:23:26 +0200

    Update ldapAliasSync.php
    Code comments

--- ldapAliasSync.php
+++ ldapAliasSync.php
@@ -7,8 +7,8 @@
  * Author: Lukas Mika <lukas.mika@web.de>
  * Licence: GPLv3. (See copying)
  */
-class ldapAliasSync extends rcube_plugin
-{
+class ldapAliasSync extends rcube_plugin {
+    
     public $task = 'login';
 
     // Internal variables
@@ -38,10 +38,8 @@ class ldapAliasSync extends rcube_plugin
     private $attr_sig;
     private $fields;
 
-    function init()
-    {
-        try
-        {
+    function init() {
+        try {
             write_log('ldapAliasSync', 'Initialising');
             
             # Load default config, and merge with users' settings
@@ -76,37 +74,30 @@ class ldapAliasSync extends rcube_plugin
             $this->separator     = $this->mail['dovecot_impersonate_seperator'];
             $this->remove_domain = $this->mail['remove_domain'];
 
+            # LDAP Connection
             $this->conn = ldap_connect($this->server);
 
-            if ( is_resource($this->conn) )
-            {
+            if ( is_resource($this->conn) ) {
                 ldap_set_option($this->conn, LDAP_OPT_PROTOCOL_VERSION, 3);
 
                 $bound = ldap_bind($this->conn, $this->bind_dn, $this->bind_pw);
 
-                if ( $bound )
-                {
-                    // Create signature
+                if ( $bound ) {
+                    # register hook
                     $this->add_hook('user2email', array($this, 'user2email'));
                     $this->initialised = true;
-                }
-                else
-                {
+                } else {
                     $log = sprintf("Bind to server '%s' failed. Con: (%s), Error: (%s)",
                         $this->server,
                         $this->conn,
                         ldap_errno($this->conn));
                     write_log('ldapAliasSync', $log);
                 }
-            }
-            else
-            {
+            } else {
                 $log = sprintf("Connection to the server failed: (Error=%s)", ldap_errno($this->conn));
                 write_log('ldapAliasSync', $log);
             }
-        }
-        catch ( Exception $exc )
-        {
+        } catch ( Exception $exc ) {
             write_log('ldapAliasSync', 'Fail to initialise: '.$exc->getMessage());
         }
 
@@ -121,62 +112,56 @@ class ldapAliasSync extends rcube_plugin
      * Return values:
      * email: E-mail address (or array of arrays with keys: email, name, organization, reply-to, bcc, signature, html_signature) 
      */
-    function user2email($args)
-    {
-        $login = $args['user'];          # User login
-        $first = $args['first'];         # True if one entry is expected
-        $extended = $args['extended'];   # True if array result (email and identity data) is expected
-        $email = $args['email'];
+    function user2email($args) {
+        $login    = $args['user'];      # User login
+        $first    = $args['first'];     # True if one entry is expected
+        $extended = $args['extended'];  # True if array result (email and identity data) is expected
+        $email    = $args['email'];
 
         # ensure we return valid information
         $args['extended'] = true;
-        $args['first'] = false;
-        $args['abort'] = false;
+        $args['first']    = false;
+        $args['abort']    = false;
 
-        try
-        {
+        try {
             # if set to true, the domain name is removed before the lookup 
-            if ( $remove_domain )
-            {            
+            if ( $remove_domain ) {            
                 $login = array_shift(explode('@', $login));
-            }
-            else
-            {
+            } else {
                 # check if we need to add a domain if not specified in the login name
-                if ( !strstr($login, '@') && $domain )
-                {
+                if ( !strstr($login, '@') && $domain ) {
                     $login = "$login@$domain" ;
                 }
             }
 
             # Check if dovecot master user is used. Use the same configuration name than
             # dovecot_impersonate plugin for roundcube
-            if ( strpos($login, $separator) !== false )
-            {   
+            if ( strpos($login, $separator) != false ) {   
                 $log = sprintf("Removed dovecot impersonate separator (%s) in the login name", $separator);
                 write_log('ldapAliasSync', $log);
 
                 $login = array_shift(explode($separator, $login));
             }   
 
+            # Replace placeholder with login
             $ldap_filter = sprintf($this->filter, $login);
+            
+            # Search for LDAP data
             $result = ldap_search($this->conn, $this->domain, $ldap_filter, $this->fields);
 
-            if ( $result )
-            {
+            if ( $result ) {
                 $info = ldap_get_entries($this->conn, $result);
 
-                if ( $info['count'] >= 1 )
-                {
+                if ( $info['count'] >= 1 ) {
                     $log = sprintf("Found the user '%s' in the database", $login);
                     write_log('ldapAliasSync', $log);
 
                     $identities = array();
 
-                    foreach ( $result as $ldapID )
-                    {
-                        $email = $ldapID[$attr_mail];
-                        $name = $ldapID[$attr_name];
+                    # Collect the identity information
+                    foreach ( $result as $ldapID ) {
+                        $email        = $ldapID[$attr_mail];
+                        $name         = $ldapID[$attr_name];
                         $organisation = $ldapID[$attr_org];
                         $reply        = $ldapID[$attr_reply];
                         $bcc          = $ldapID[$attr_bcc];
@@ -189,13 +174,10 @@ class ldapAliasSync extends rcube_plugin
                         if ( !$bcc )          $bcc          = '';
                         if ( !$signature )    $signature    = '';
                         
-                        
-                        if ( preg_match('/^\s*<[a-zA-Z]+/', $signature) )
-                        {
+                        # If the signature starts with an HTML tag, we mark the signature as HTML
+                        if ( preg_match('/^\s*<[a-zA-Z]+/', $signature) ) {
                             $isHtml = 1;
-                        }
-                        else
-                        {
+                        } else {
                             $isHtml = 0;
                         }
 
@@ -211,38 +193,35 @@ class ldapAliasSync extends rcube_plugin
                             
                         array_push($identities[], $identity);
                     }
-
+                    
+                    # Return structure for our LDAP identities
                     $args['email'] = $identities;
                     
-                    if (count($identities[]) > 0 && $db_identities[] = $this->app->user->list_identities())
-                    {
-                        foreach ($db_identities as $db_identity)
-                        {
+                    # Check which identities are available in database but nut in LDAP and delete those
+                    if (count($identities[]) > 0 && $db_identities[] = $this->app->user->list_identities()) {
+                        foreach ($db_identities as $db_identity) {
                             $in_ldap = null;
-                            foreach ($identities as $identity)
-                            {
-                                if($db_identity['email'] == $identity['email'] && !$in_ldap)
-                                {
+                            
+                            foreach ($identities as $identity) {
+                                # email is our only comparison parameter
+                                if($db_identity['email'] == $identity['email'] && !$in_ldap) {
                                     $in_ldap = $db_identity['identity_id'];
                                 }
                             }
-                            if (!$in_ldap)
-                            {
+                            
+                            # If this identity does not exist in LDAP, delete it from database
+                            if (!$in_ldap) {
                                 $db_user->delete_identity($in_ldap);
                             }
                         }
                         $log = sprintf("Identities synced for %s", $login);
                         write_log('ldapAliasSync', $log);
                     }
-                }
-                else
-                {
+                } else {
                     $log = sprintf("User '%s' not found (pass 2). Filter: %s", $login, $filter);
                     write_log('ldapAliasSync', $log);
                 }
-            }
-            else
-            {
+            } else {
                 $log = sprintf("User '%s' not found (pass 1). Filter: %s", $login, $filter);
                 write_log('ldapAliasSync', $log);
             }
@@ -251,6 +230,5 @@ class ldapAliasSync extends rcube_plugin
         }
         return $args;
     }
-
 }
 ?>
