8bc7fe1bc5a6d024952bc33797fb8717ca1e2ee5
Lukas Mika <lukas.mika@web.de>
2013-07-10 11:39:54 +0200

    Update config-default.inc.php
    find domain

--- config-default.inc.php
+++ config-default.inc.php
@@ -5,18 +5,19 @@
 $rcmail_config['ldapAliasSync'] = array(
     // Mail parameters
     'mail' => array(
-        'domain' => 'example.com',                          // If necessary, you can specify a domain to add after the login name
-        'dovecot_impersonate_seperator' => '*'              // If using dovecot master users, remove the admin name before the lookup
-        'remove_domain' => true,                            // if set to true, the domain name (eg. xxxx@example.com) is removed before the lookup
+        'remove_domain'     => true,            // remove domain part from login name (xyz@example.com --> xyz)
+        'search_domain'     => 'example.com',   // add domain to login (xyz --> xyz@example.com) (optional)
+        'find_domain'       => 'example.com',   // add domain to found local parts (xyz --> xyz@example.com) (optional)
+        'dovecot_seperator' => '*',             // remove admin name (dovecot master user) at login (optional)
     ),
 
     // LDAP parameters
     'ldap' => array(
-        'server'     => 'ldap://localhost',                                         // Your LDAP server address (required)
+        'server'     => 'ldap://localhost',                                         // LDAP server address (required)
         'bind_dn'    => 'cn=mail,dc=example,dc=com',                                // LDAP Bind DN (optional)
         'bind_pw'    => 'secret',                                                   // Bind password (optional)
         'base_dn'    => 'ou=aliases,dc=example,dc=com',                             // LDAP search base (required)
-        'filter'     => '(aliasedobjectname=uid=%s,ou=users,dc=example,dc=org)',    // The LDAP filter to use
+        'filter'     => '(aliasedobjectname=uid=%s,ou=users,dc=example,dc=org)',    // LDAP search filter (required)
         'attr_mail'  => 'uid',                                                      // LDAP email attribute (required)
                                                                                     //  --> can also be local part
         'attr_name'  => 'cn',                                                       // LDAP name attribute (optional)
