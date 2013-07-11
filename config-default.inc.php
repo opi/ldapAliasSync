a11cd774b24808e35880bb0cc3569ee577727920
Lukas Mika <lukas.mika@web.de>
2013-07-11 11:14:47 +0200

    Update config-default.inc.php
    Parameter comments & filter enhancement

--- config-default.inc.php
+++ config-default.inc.php
@@ -1,30 +1,77 @@
 <?php
-// Default configuration settings for ldapAliasSync
-// Copy this file in config.inc.php, and override the values you need.
+/*
+ * Default configuration settings for ldapAliasSync roundcube plugin
+ * Copy this file in config.inc.php, and override the values you need.
+*/
 
 $rcmail_config['ldapAliasSync'] = array(
     // Mail parameters
     'mail' => array(
-        'remove_domain'     => true,            // remove domain part from login name (xyz@example.com --> xyz)
-        'search_domain'     => 'example.com',   // add domain to login (xyz --> xyz@example.com) (optional)
-        'find_domain'       => 'example.com',   // add domain to found local parts (xyz --> xyz@example.com) (optional)
-        'dovecot_seperator' => '*',             // remove admin name (dovecot master user) at login (optional)
+        # Remove domain part from login name (xyz@example.com --> xyz) if given
+        # Set to true, if you intend to only lookup the local part in LDAP;
+        # set to false, if you intend to lookup the whole email address.
+        'remove_domain'     => true,
+        
+        # Domain to add to login (xyz --> xyz@example.com) if none is given (optional)
+        # This parameter is needed, if users login with only their local parts (xyz),
+        # but you intend to query the LDAP for the whole email address
+        'search_domain'     => 'example.com',
+        
+        # Domain to add to found local parts (asdf --> asdf@example.com) (optional)
+        # If the returned value does only contain the local part of an email address,
+        # this domain will be used as the domain part.
+        # This may only be empty, if all identities to be found contain a domain part
+        # in their email addresses as all identities without a domain part in the email
+        # address will not be returned!
+        'find_domain'       => 'example.com',
+        
+        # Dovecot master user seperator (optional)
+        # If you use the dovecot impersonation feature, this seperator will be used
+        # in order to determine the actual login name.
+        # Set it to the same character if using this feature, otherwise you can also
+        # leave it empty.
+        'dovecot_seperator' => '*',
     ),
 
     // LDAP parameters
     'ldap' => array(
-        'server'     => 'ldap://localhost',                                         // LDAP server address (required)
-        'bind_dn'    => 'cn=mail,dc=example,dc=com',                                // LDAP Bind DN (optional)
-        'bind_pw'    => 'secret',                                                   // Bind password (optional)
-        'base_dn'    => 'ou=aliases,dc=example,dc=com',                             // LDAP search base (required)
-        'filter'     => '(aliasedobjectname=uid=%s,ou=users,dc=example,dc=org)',    // LDAP search filter (required)
-        'attr_mail'  => 'uid',                                                      // LDAP email attribute (required)
-                                                                                    //  --> can also be local part
-        'attr_name'  => 'cn',                                                       // LDAP name attribute (optional)
-        'attr_org'   => 'o',                                                        // LDAP organization attribute (optional)
-        'attr_reply' => '',                                                         // LDAP reply-to attribute (optional)
-        'attr_bcc'   => '',                                                         // LDAP bcc attribute (optional)
-        'attr_sig'   => '',                                                         // LDAP signature attribute (optional)
+        # LDAP server address (required)
+        'server'     => 'ldap://localhost',
+        
+        # LDAP Bind DN (optional)
+        'bind_dn'    => 'cn=mail,dc=example,dc=com',
+        
+        # Bind password (optional)
+        'bind_pw'    => 'secret',
+        
+        # LDAP search base (required)
+        'base_dn'    => 'ou=aliases,dc=example,dc=com',
+        
+        # LDAP search filter (required)
+        # This open filter possibility is the heart of the LDAP search.
+        # Use '%1$s' as a place holder for the login name (domain part depending on the configuration above)
+        # However, remember to search for the original entry, too (e.g. 'uid=%1$s'), as this is an identity as well!
+        'filter'     => '(|(uid=%1$s)(aliasedobjectname=uid=%1$s,ou=users,dc=example,dc=org)',
+        
+        # LDAP email attribute (required)
+        # If only the local part is returned, the 'find_domain' is appended (e.g. uid=asdf --> asdf@example.com).
+        # If no domain part is returned and no 'find_domain' is given, the identity will not be fetched!
+        'attr_mail'  => 'uid',
+        
+        # LDAP name attribute (optional)
+        'attr_name'  => 'cn',
+        
+        # LDAP organization attribute (optional)
+        'attr_org'   => 'o',
+        
+        # LDAP reply-to attribute (optional)
+        'attr_reply' => '',
+        
+        # LDAP bcc (blind carbon copy) attribute (optional)
+        'attr_bcc'   => '',
+        
+        # LDAP signature attribute (optional)
+        'attr_sig'   => '',
     ),
 );
 ?>
